895758a7a17ff9a0bceb69d167aef28b47b0d596
Alejandro <alejandro.gemas@gmail.com>
2019-11-06 19:24:05 +0100

    Añadido Camara a medias

--- OpenGLEngineTFG/OpenGLEngineTFG/Camara/Camara.cpp
+++ OpenGLEngineTFG/OpenGLEngineTFG/Camara/Camara.cpp
@@ -1,5 +1,96 @@
 #include "Camara.h"
 
-Camara::Camara() {}
+#include <gtc/matrix_transform.hpp>
+
+Camara::Camara(float fov, int width, int height, float zNear, float zFar): mView(glm::mat4(1.0f)), fov(fov), zNear(zNear), zFar(zFar) {
+
+	//Datos para el ratón
+	this->lastX = 400;
+	this->lastY = 300;
+
+	this->yaw = -15.0;
+	this->pitch = -70.0;
+
+	this->mProjection = glm::perspective( glm::radians(fov), width / float(height), zNear, zFar);
+
+	// Parsear desde Data/Config??
+	this->vecPositionCamera = glm::vec3(0.0f, 0.0f, -3.0f);
+	this->vecLookAt = glm::vec3(0.0f, 0.0f, 10.0f);
+	this->vecUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+	this->mView = glm::lookAt(vecPositionCamera, vecLookAt, vecUp);
+}
 
 Camara::~Camara() {}
+
+
+void Camara::moveCamara(float xPosition, float yPosition)
+{
+	//Actualizamos las posiciones para la proxima iteración
+	this->lastX = xPosition;
+	this->lastY = yPosition;
+
+	// Obtenemos el yaw y pitch.
+	this->yaw += (xPosition - this->lastX) * 0.05;
+	this->pitch += (this->lastY - yPosition) * 0.05;
+
+
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	// Spherical coordinates (r=1).
+	n.x = cos(glm::radians(pitch)) * cos(glm::radians(yaw));
+	n.y = sin(glm::radians(pitch));
+	n.z = cos(glm::radians(pitch)) * sin(glm::radians(yaw));
+
+	this->n = normalize(n);
+	this->u = normalize(cross(n, glm::vec3(0, 1, 0)));
+	this->v = normalize(cross(u, n));
+	this->vecLookAt = vecPositionCamera + n;
+
+	this->View = glm::lookAt(vecPositionCamera, vecLookAt, v);
+}
+
+/* Comprueba si se ha pulsado las teclas para actualizar su estado*/
+void Camara::UpdateCamera()
+{
+	if (InputManager::getInstance()->getInputButtonDown(Key_A)) {
+		// TODO
+	}
+
+	if (InputManager::getInstance()->getInputButtonDown(Key_D)) {
+		// TODO
+	}
+
+	if (InputManager::getInstance()->getInputButtonDown(Key_W)) {
+		// TODO
+	}
+
+	if (InputManager::getInstance()->getInputButtonDown(Key_S)) {
+		// TODO
+	}
+}
+
+/* Cambia la matriz de proyeccion según el nuevo alto y ancho que tenga la ventana */
+void Camara::SetProjection(float newWidth, float newHeight)
+{
+	this->mProjection = glm::perspective( glm::radians( this->fov), newWidth / float(newHeight), this->zNear, this->zFar );
+}
+
+/* Getters */
+glm::mat4 Camara::getView()
+{
+	return this->mView;
+}
+
+glm::mat4 Camara::getProjection()
+{
+	return this->getProjection;
+}
+
+glm::mat4 Camara::getVP()
+{
+	return this->mProjection * this->mView;
+}
--- OpenGLEngineTFG/OpenGLEngineTFG/Camara/Camara.h
+++ OpenGLEngineTFG/OpenGLEngineTFG/Camara/Camara.h
@@ -1,14 +1,44 @@
 #pragma once
 
 #include <glm.hpp>
-#include <gtc/matrix_transform.hpp>
+
+#include "../Input/InputManager.h"
 
 class Camara
 {
 public:
-	Camara();
+	Camara(float fov, int width, int height, float zNear, float zFar);
 	~Camara();
 
-private:
+	/* Funciones públicas */
+	void UpdateCamera(); // Comprueba si se ha pulsado las teclas para actualizar su estado
+	void SetProjection(float newWidth, float newHeight);
+	void moveCamara(float xPosition, float yPosition);
 
+	/* Getters */
+	glm::mat4 getView();
+	glm::mat4 getProjection();
+	glm::mat4 getVP();
+
+private:
+	// Matrices para el render
+	glm::mat4 mView;
+	glm::mat4 mProjection;
+
+	//Datos de la cámara (Posición, a donde mira y vector de arriba)
+	glm::vec3 vecPositionCamera;
+	glm::vec3 vecLookAt;
+	glm::vec3 vecUp;
+
+	float fov;
+	float zNear;
+	float zFar;
+
+	//Datos para el ratón
+	bool primerMovRaton;
+	float lastX;
+	float lastY;
+
+	float yaw;
+	float pitch;
 };
